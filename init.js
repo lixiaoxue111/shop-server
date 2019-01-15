ebba88b653bc3eab9eda5bf0fb3be14886ed8772
newlls <523786175@qq.com>
2019-01-15 14:59:28 +0800

    init

---
+++ init.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const db = 'mongodb://localhost/shop';//默认端口27017
+//引入 schema
+const glob = require('glob');
+const path = require('path');
+exports.initSchemas = () => {
+    //获取当前项目的绝对地址
+    glob.sync(path.resolve(__dirname, './model', '*.js')).forEach(require);
+};
+exports.connect =() => {
+    //连接数据库
+    mongoose.connect(db,{useNewUrlParser:true});
+    //监听数据库连接， 失败重连
+    mongoose.connection.on('disconnected',()=>{
+        mongoose.connect(db);
+    });
+    //数据库出现错误
+    mongoose.connection.on('error',err=> {
+        console.log(err);
+        mongoose.connect(db);
+    });
+    //连接成功
+    mongoose.connection.once('open',()=>{
+        console.log('connect success');
+    })
+};
